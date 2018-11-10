b4b65f88fcaec7ea619bd1ed1cc0ea7319597106
wiwilvey <wilveywhye@gmail.com>
2018-11-10 14:47:13 +0700

    tampilan awal

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,26 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
+var server="https://klubaners.web.id/wb/";
+$(document).ready( function(){
+	//ambil daftar destinsi dari server
+	$.getJSON(
+		server+"destinasiDft.php",
+		function(deswi){
+			// kosongkan div id=destinasi dari elemen li
+			$("#destinasi li").remove();
+			
+			// olah variabel deswi, hasil output dari server
+			$.each(deswi, function(i,ow){
+				//tambahkan li ke div id=destinasi
+				// nama key / kunci variabel deswi adalah:
+				// dObjWisata,namaObj,desa,kecamatan, dan koordinatPeta
+				$("#destinasi").append(
+					"<li class='list-group-item'>"+
+					"<p class='namaOw'>"+ow.namaObj+"</p>"+
+					"<p><label>Kecamatan :</label>"+ow.kecamatan+"</p>"+
+					"<p><label>Desa :</label>"+ow.desa+"</p>"+
+					"<p class='lokasiOw'><label>Lokasi GPS :</label>"+ow.koordinatPeta+"</p>"+
+					"</li>"
+				);
+			});
+		}
+	);
+})
